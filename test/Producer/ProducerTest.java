fbe33a90a327193dafde399d53dc790f67273d03
vaxtomis <1051551978@qq.com>
2021-08-17 17:39:04 +0800

    ProducerTest.

--- test/Producer/ProducerTest.java
+++ test/Producer/ProducerTest.java
@@ -1,28 +1,38 @@
 package Producer;
 
-import Parser.*;
+import Parser.Parser;
 import org.junit.Test;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author vaxtomis
  */
 public class ProducerTest {
+    Parser parser = new Parser();
+    Producer producer = new Producer();
+    Map<String, ?> map = new HashMap<>();
     @Test
     public void producerTest() {
         long start=System.currentTimeMillis();
-        Parser parser = new Parser("test/test.yml");
-        parser.loopProcessing();
-        LinkedList<Event> events = parser.getEventList();
-        Producer producer = new Producer(events);
+        parser.setPath("test/test.yml");
+        producer.setInnerMap(map);
+        producer.setEvents(parser.getEventList());
         producer.build();
-        System.gc();
         long end=System.currentTimeMillis();
         System.out.println("程序运行时间： "+(end-start)+"ms");
-        Map map = producer.getInnerMap();
-        classA a = (classA) map.get("A1");
-        System.out.println(a.toString());
+        classA A1 = (classA) map.get("A1");
+        classA A2 = (classA) map.get("A2");
+        classA A3 = (classA) map.get("A3");
+        classA A4 = (classA) map.get("A4");
+        classA A5 = (classA) map.get("A5");
+        classA A6 = (classA) map.get("A6");
+        System.out.println(A1.toString());
+        System.out.println(A2.toString());
+        System.out.println(A3.toString());
+        System.out.println(A4.toString());
+        System.out.println(A5.toString());
+        System.out.println(A6.toString());
     }
 }
